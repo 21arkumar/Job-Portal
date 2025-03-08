2e5d8e013df1028f3f1ebe6fa1dcb499d7255bdf
21arkumar <21arkumar@gmail.com>
2025-03-09 04:21:40 +0530

    Header Creation

--- jobportal/src/App.jsx
+++ jobportal/src/App.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import './App.css'
 
 export default class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+        <div id="container">
+            <div id="header">
+                <img className='logo' src="/logo.png" alt="" />
+                <div className='logoTxt'><span>Job</span> Portal</div>
+                <img className='signinIcon' src='/user.png' alt=''/>
+                <label className='signinText'>Sign In</label>
+            </div>
+
+            <div id="content">
+                content
+            </div>
+
+            <div id="footer">
+                footer
+            </div>
+        </div>
     )
   }
 }
--- jobportal/src/main.jsx
+++ jobportal/src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,9 +6,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path = '/' element = {<App />}>
-
-      </Route>
+      <Route path = '/' element = {<App />}/>
     </Routes>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
